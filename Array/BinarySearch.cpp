229ddeadd235084eeb02612c47b1ae051cf394f0
omch-29 <chandwaniom29@gmail.com>
2025-11-16 22:04:39 +0530

    BS

--- Array/BinarySearch.cpp
+++ Array/BinarySearch.cpp
@@ -6,3 +6,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+int BS(vector<int>& nums, int tar){
+    int st=0,end=nums.size();
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(nums[mid]<tar) st++;
+        else if(nums[mid]>tar) end--;
+        else return mid+1;
+    }
+    return -1;
+}
+int main(){
+    vector<int>nums = {1,2,4,5,6,7};
+    cout<<BS(nums,1);
+}
