cefb505a23e1a9073b23c69028702490f9c37b8e
omch-29 <chandwaniom29@gmail.com>
2025-11-18 22:33:19 +0530

    array

---
+++ 2dArray/spiralMat.cpp
@@ -0,0 +1,28 @@
+//given matrix of n*m
+//to print in spiral manner-> top->right->bottom->left
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+int spiral(int mat[][4], int n, int m){
+    int srow=0,erow=n-1,scol=0,ecol=m-1;
+    while(srow<=erow && scol<=ecol){
+    for(int j=scol;j<=ecol;j++){
+        cout<<mat[srow][j]<<"";
+    }
+    for(int i=srow+1;i<=erow;i++){
+        cout<<mat[i][ecol]<<"";
+    }
+    for(int j=ecol-1;j>=scol;j--){
+        if(srow==erow) break;
+        cout<<mat[erow][j]<<"";
+    }
+    for(int i=erow-1;i>=srow+1;i--){
+        if(scol==ecol) break;
+        cout<<mat[i][scol]<<"";
+    }
+    srow++,scol++;
+    erow--,ecol--;
+}
+}
