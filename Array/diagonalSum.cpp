f17c538619c377f9bf3bceca4c26b8648c77105a
omch-29 <chandwaniom29@gmail.com>
2025-11-19 15:39:57 +0530

    array

--- Array/diagonalSum.cpp
+++ Array/diagonalSum.cpp
@@ -16,7 +16,17 @@ int diag(int mat[][3], int n){
     }
     return sum;
 }
+int diag2(int mat[][3], int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=mat[i][i];
+        if(i!=n-i-1) sum+=mat[i][n-i-1];
+    }
+    return sum;
+}
 int main(){
    int mat[3][3]={{1,2,3},{5,6,7},{9,10,11}};
    cout<<diag(mat,3);
+   cout<<endl;
+   cout<<diag2(mat,3);
 }
