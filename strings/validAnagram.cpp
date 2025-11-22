ba1c935e1f634cc1fef9799437c5abaf9a3320f4
omch-29 <chandwaniom29@gmail.com>
2025-11-22 16:28:20 +0530

    strings

--- strings/validAnagram.cpp
+++ strings/validAnagram.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <vector>
 #include<cstring>
+#include<unordered_map>
 #include <climits>
 using namespace std;
 bool ana(string s1, string s2){
+    unordered_map<char,int>freq;
+    
     if(s1.length()!=s2.length()) return false;
-    int count[26]={0};
-    for(int i=0;i<s1.length();i++){
-        int idx=s1[i]-'a';
-        count[idx]++;
+    for(char c:s1){
+        freq[c]++;
     }
-    for(int i=0;i<s2.length();i++){
-        int idx=s2[i]-'a';
-        if(count[idx]==0) return false;
-        count[idx]--;
+    for(char c:s2){
+        if(freq[c]==0) return false;
+        freq[c]--;
     }
+    // int count[26]={0};
+    // for(int i=0;i<s1.length();i++){
+    //     int idx=s1[i]-'a';
+    //     count[idx]++;
+    // }
+    // for(int i=0;i<s2.length();i++){
+    //     int idx=s2[i]-'a';
+    //     if(count[idx]==0) return false;
+    //     count[idx]--;
+    // }
     return true;
 }
 int main(){
     string s1="hey";
-    string s2="yoh";
+    string s2="yeh";
     cout<<ana(s1,s2);
 }
