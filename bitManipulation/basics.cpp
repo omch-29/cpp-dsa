cdcb4c278fc213966f4acc8a6884677ce93bca31
omch-29 <chandwaniom29@gmail.com>
2025-11-23 19:42:23 +0530

    BIT

--- bitManipulation/basics.cpp
+++ bitManipulation/basics.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 int main(){
     cout<<~(6)<<endl;
-    //now,  -6 = 0000 0110, 1s C=1111 1001, here 1st 1=signBit=-ve, = 111 1001, now to find its magnitude.. we need to find 2's C
-    //2's C=find 1s C then add 1, 1s C of 111 1001 = 000 0110 now +1 = 000 0111 = 7 and since signBit is -ve therefore its -7
+ //now,  -6 = 0000 0110, 1s C=1111 1001, here 1st 1=signBit=-ve, = 111 1001, now to find its magnitude.. we need to find 2's C.
+//2's C=find 1s C then add 1, 1s C of 111 1001 = 000 0110 now +1 = 000 0111 = 7 and since signBit is -ve therefore its -7
+//thus, ~x = its 1S c => pick signBit => 2s C of found 1s C => magnitude with signbit.
+cout<<(7<<2)<<endl;
 }
