8951eea43a1023f318740a277162c001bc6c40a6
omch-29 <chandwaniom29@gmail.com>
2025-11-24 21:11:30 +0530

    array

--- Array/ReverseArray.cpp
+++ Array/ReverseArray.cpp
@@ -1,6 +1,7 @@
 //Without extra space
 
 #include <iostream>
+#include <climits>
 #include <vector>
 using namespace std;
 
@@ -17,8 +18,20 @@ int ra(int arr[], int n){
         cout<<arr[i]<<endl;
     }
 }
+//with extra space
+int esra(int arr[], int n){
+    int earr[n];
+    int j=0;
+    for(int i=n-1;i>=0;i--){
+        earr[j++]=arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<earr[i];
+    }
+}
 int main(){
     int arr[]={8,2,3,4,5};
     int n=5;
-    cout<<ra(arr,n);
+    // cout<<ra(arr,n);
+    cout<<esra(arr,n);
 }
