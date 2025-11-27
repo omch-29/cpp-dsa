0b9afa2f1a71dc219ca3b2cd69c8f05863480fab
omch-29 <chandwaniom29@gmail.com>
2025-11-27 17:05:16 +0530

    2sum

--- Array/2sum.cpp
+++ Array/2sum.cpp
@@ -7,12 +7,18 @@ using namespace std;
 
 pair<int,int> twosum(vector<int>& nums, int tar){
     int n=nums.size();
+    unordered_map<int,int>mp;
     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(nums[i]+nums[j]==tar) return {i, j};
-
-        }
+        int sec=tar-nums[i];
+        if(mp.find(sec)!=mp.end()) return {i,mp[sec]};
+        mp[nums[i]]=i;
     }
+    // for(int i=0;i<n;i++){
+    //     for(int j=i+1;j<n;j++){
+    //         if(nums[i]+nums[j]==tar) return {i, j};
+
+    //     }
+    // }
     return {-1, -1};
 }
 
