351cf83672ec7a6b08f676beeea303b997362cb3
omch-29 <chandwaniom29@gmail.com>
2025-11-29 19:27:48 +0530

    oops

--- oops.cpp
+++ oops.cpp
@@ -42,8 +42,19 @@ public:
         cout<<"ended\n";
     }
 };
-//Encapsulation: wrapping up data and member functions(methods) in a single unit. It also implements data hiding.
-//Constructor: Special method invoked automatically during object creation.
+//1]Encapsulation: wrapping up data and member functions(methods) in a single unit. It also implements data hiding.
+//Constructor: Special method invoked automatically during object creation=> allocates memory., Destructor: deallocates memory(~dest, also auto)
+//shallow copy : copies refernces to original array
+//deep copy: creates new copy of array., compiler generally creates shallow copy.
+//2]Inheritance : when properties and methods from parent class are passed to child class. class child : public parent, 3types: simgle, multi-level: p->c->c, multiple inher: myltiple parent, simgle child.
+//3]Polymorphism : Ability of object to take different forms => abil to behave in diff ways depending upon context. for ex: two functions with same name 
+//but different data type of params.
+//Funcn overloading: same name, different params type.
+//operator overloading : changing logic for operators.
+//funcn overriding : whne parent class and C class contain same funcn with diff implementn, P class f is overriden.
+//                funcn overloading     vs funcn overriding
+//compile type polymorphism         ||   run time polymorphism
+//single class has same funcn           P and C class ahve same .....
 int main(){
     // Student s1;
     // s1.setName("me");
