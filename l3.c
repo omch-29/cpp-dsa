4c7bd3baad0043ee30b0a75d6e3ffe2704a0d101
omch-29 <chandwaniom29@gmail.com>
2025-12-07 19:27:30 +0530

    .c

---
+++ l3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int i,j,temp,n,table[40][10]={0};
+    char set[20];
+    printf("\nEnter number of elemnts");
+    scanf("%d",&n);
+    printf("\nEnter the set elements:");
+    for(int i=0;i<n;i++){
+        scanf("%c",&set[i]);
+    }
+}
