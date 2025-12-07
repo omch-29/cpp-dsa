695745aeffb955992c72f547ab100f03889848bc
omch-29 <chandwaniom29@gmail.com>
2025-12-07 19:41:31 +0530

    .c

--- l3.c
+++ l3.c
@@ -6,7 +6,30 @@ int main(){
     printf("\nEnter number of elemnts");
     scanf("%d",&n);
     printf("\nEnter the set elements:");
-    for(int i=0;i<n;i++){
-        scanf("%c",&set[i]);
+    for(i=0;i<n;i++){
+        scanf(" %c",&set[i]);
+    }
+    printf("\nBinary representation:\n");
+    for(i=0;i<pow(2,n);i++){
+        temp=i;
+        for(j=0;j<n;j++){
+            table[i][j]=temp%2;
+            temp/=2;
+            printf("%d",table[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\npower set is:\n");
+    for(i=0;i<pow(2,n);i++){
+        int first=1;
+        printf("{");
+        for(j=0;j<n;j++){
+            if(table[i][j]==1){
+                if(!first) printf(",");
+                printf("%c",set[j]);
+                first=0;
+            }
+        }
+        printf("}\n");
     }
 }
