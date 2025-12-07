1dd4317bcd96843205c0095ebe1a846589c9cb3b
omch-29 <chandwaniom29@gmail.com>
2025-12-07 19:58:46 +0530

    .c

--- l3.c
+++ l3.c
@@ -1,34 +1,27 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int i,j,temp,n,table[40][10]={0};
+    int n;
     char set[20];
-    printf("\nEnter number of elemnts");
+    printf("Enter number of elements:");
     scanf("%d",&n);
-    printf("\nEnter the set elements:");
-    for(i=0;i<n;i++){
+    printf("\nEnter elemnets\n");
+    for(int i=0;i<n;i++){
         scanf(" %c",&set[i]);
     }
-    printf("\nBinary representation:\n");
-    for(i=0;i<pow(2,n);i++){
-        temp=i;
-        for(j=0;j<n;j++){
-            table[i][j]=temp%2;
-            temp/=2;
-            printf("%d",table[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\npower set is:\n");
-    for(i=0;i<pow(2,n);i++){
-        int first=1;
+    int total=pow(2,n);
+    printf("\nPower set is:");
+    for(int i=0;i<total;i++){
         printf("{");
-        for(j=0;j<n;j++){
-            if(table[i][j]==1){
+        int first=1;
+        int mask=i;
+        for(int j=0;j<n;j++){
+            if(mask%2){
                 if(!first) printf(",");
                 printf("%c",set[j]);
                 first=0;
             }
+            mask/=2;
         }
         printf("}\n");
     }
