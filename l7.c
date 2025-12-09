2a64c899ef4b4c2323ce722f946899fd7d7f59a4
omch-29 <chandwaniom29@gmail.com>
2025-12-09 18:07:25 +0530

    l7

--- l7.c
+++ l7.c
@@ -1,12 +1,26 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int nums[10]={1,2,3,4,5,6,7,8,9,10};
-    int st=0,end=10-1;
-    int tar=4;
+    int n,tar,f=0;
+    printf("Enter size of array:");
+    scanf("%d",&n);
+    int nums[n];
+    printf("enter elements");
+    for(int i=0;i<n;i++) scanf("%d",&nums[i]);
+    printf("enter target:");
+    scanf("%d",&tar);
+    int st=0,end=n-1;
     while(st<=end){
         int mid=st+(end-st)/2;
-        if(nums[mid]==tar) return mid;
-        if
+        if(nums[mid]==tar){
+            f=1;
+         printf("Position of %d is %d",tar,mid+1);
+         break;
+        }
+        if(nums[mid]<tar) st=mid+1;
+        else end=mid-1;
+    }
+    if(!f){
+        printf("inv");
     }
 }
