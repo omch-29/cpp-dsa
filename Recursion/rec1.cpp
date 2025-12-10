7d4a7d2f6f3d05bb186dd842d758d10c91fd6c71
omch-29 <chandwaniom29@gmail.com>
2025-12-10 20:31:12 +0530

    l7

--- Recursion/rec1.cpp
+++ Recursion/rec1.cpp
@@ -9,12 +9,12 @@ int fact(int n){
     if(n==1) return 1;
     return n*fact(n-1);
 }
-// int fib(int n){
-//     if(n==0 || n==1) return n;
-//     return fib(n-1)+fib(n-2); //at fib, n=n-1 + n-2
-// }
-// int main(){
-//     cout<<fact(6)<<endl;
-//     cout<<fact(5)<<"\n";
-//     cout<<fib(7);
-// }
+int fib(int n){
+    if(n==0 || n==1) return n;
+    return fib(n-1)+fib(n-2); //at fib, n=n-1 + n-2
+}
+int main(){
+    cout<<fact(6)<<endl;
+    cout<<fact(5)<<"\n";
+    cout<<fib(7);
+}
