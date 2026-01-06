3cb420a7b6b01b05a26161fded0054ae1f511e2d
omch-29 <chandwaniom29@gmail.com>
2026-01-06 19:02:32 +0530

    test

--- test.cpp
+++ test.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+bool isit(int n){
+    if((n/2)*2 == n) return true;
+    else return false;
+}
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5};
     int n;
     cout<<"enter integer:";
     cin>>n;
-    if((n/2)*2 == n) return true;
-    else return false;
+    cout<<isit(n);
 }
