9dec2d4c013d068adc6ed49451ad8486cada609c
omch-29 <chandwaniom29@gmail.com>
2026-01-09 08:35:10 +0530

    stack

--- stack/histry.cpp
+++ stack/histry.cpp
@@ -6,31 +6,19 @@
 #include <climits>
 using namespace std;
 int largestRectangleArea(vector<int>& heights){
-    if(heights.empty()) return 0;
-    if(heights.size()==1) return heights[0];
-    int n=heights.size();
-    vector<int>nsl(n),nsr(n);
-    stack<int>s;
-    nsl[0]=-1;
-    s.push(0);
-    for(int i=1;i<n;i++){
-        while(!s.empty() && heights[i]<=heights[s.top()]) s.pop();
-        if(s.empty()) nsl[i]=-1;
-        else nsl[i]=s.top();
-        s.push(i);
-    }
-    while(!s.empty()) s.pop();
-    s.push(n-1);
-    nsr[n-1]=n;
-    for(int i=n-2;i>=0;i--){
-        while(!s.empty() && heights[i]<=heights[s.top()]) s.pop();
-        if(s.empty()) nsr[i]=n;
-        else nsr[i]=s.top();
-        s.push(i);
-    }
-    int maxArea=0;
-    for(int i=0;i<n;i++) maxArea=max(maxArea, heights[i] * (nsr[i]-nsl[i]-1));
-    return maxArea;
+  if(heights.empty()) return 0;
+  if(heights.size()==1) return heights[0];
+  int n=heights.size();
+  vector<int>nsr(n),nsl(n);
+  stack<int>s;
+  nsl[0]=-1;
+  s.push(0);
+  for(int i=1;i<n;i++){
+    while(!s.empty() && heights[i]<=heights[s.top()]) s.pop();
+    if(s.empty()) nsl[i]=-1;
+    else nsl[i]=s.top();
+    s.push(i);
+  }
 }
 int main(){
     vector<int>heights = {2,1,5,6,2,3};
