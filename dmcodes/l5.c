1f3e2fa74e065224c1ded20df509eb3504802b0d
omch-29 <chandwaniom29@gmail.com>
2026-01-11 11:58:19 +0530

    dm

--- dmcodes/l5.c
+++ dmcodes/l5.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 #include<math.h>
-void print(int *num, int n){
+void print(int *num,int n){
     for(int i=0;i<n;i++){
         printf("%d",num[i]);
     }
     printf("\n");
 }
 int main(){
-    int n;
-    int num[10];
-    printf("Enter number of elements:");
+    int i,j,n,num[10];
+    printf("Enter size:\n");
     scanf("%d",&n);
-    printf("Enter elements:");
-    for(int i=0;i<n;i++) scanf("%d",&num[i]);
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-1;j++){
+    printf("Enter elements\n");
+    for(i=0;i<n;i++) scanf("%d",&num[i]);
+    printf("permutations are:-\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n-1;j++){
             int temp=num[j];
             num[j]=num[j+1];
             num[j+1]=temp;
             print(num,n);
         }
     }
+    return 0;
 }
