c9a85eb991e58d21635306fce0a0fba9fc3764c1
omch-29 <chandwaniom29@gmail.com>
2026-01-13 15:47:37 +0530

    tries

--- tries/building.cpp
+++ tries/building.cpp
@@ -29,9 +29,25 @@ public:
         }
         temp->endOfWord = true;
     }
+    bool search(string key){                        //O(L)
+        Node* temp = root;
+        for(int i=0;i<key.size();i++){
+            if(temp->children.count(key[i])){
+                temp = temp->children[key[i]];
+            }else{
+                return false;
+            }
+        }
+        return temp->endOfWord;
+    }
 };
 
 int main(){
     vector<string> words= {"the", "e", "there", "their", "any", "thee"};
     Trie trie;
+    for(int i=0;i<words.size();i++){
+        trie.insert(words[i]);
+    }
+    cout<<trie.search("their")<<endl;
+    return 0;
 }
