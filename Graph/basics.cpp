428b01583e9e9103dd51084d1802ae256db5b44e
omch-29 <chandwaniom29@gmail.com>
2026-01-15 22:32:03 +0530

    graph

--- Graph/basics.cpp
+++ Graph/basics.cpp
@@ -12,8 +12,29 @@ public:
         l = new list<int> [V];
         
     }
+    void addEdge(int u, int v) {
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+    void print(){
+        for(int u=0;u<V;u++){
+            list<int> neighbors = l[u];
+            cout<< u << ":";
+            for(int v : neighbors){
+                cout<< v << " ";
+            }
+            cout<<endl;
+        }
+    }
  };
  int main(){
-    Graph graph;
+    Graph graph(5);
+    graph.addEdge(0,1);
+    graph.addEdge(1,2);
+    graph.addEdge(1,3);
+    graph.addEdge(2,3);
+    graph.addEdge(2,4);
+
+    graph.print();
     return 0;
  }
