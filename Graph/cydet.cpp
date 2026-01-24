2685f1120ea2018765c2003966e08a45c7fccd82
omch-29 <chandwaniom29@gmail.com>
2026-01-24 19:06:15 +0530

    graph

--- Graph/cydet.cpp
+++ Graph/cydet.cpp
@@ -75,7 +75,7 @@ public:
 int main(){
     Graph graph(5);
     graph.addEdge(0,1);
-    // graph.addEdge(0,2);
+    graph.addEdge(0,2);
     graph.addEdge(0,3);
     graph.addEdge(1,2);
     graph.addEdge(3,4);
