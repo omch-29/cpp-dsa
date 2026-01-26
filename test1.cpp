d6023ad912c94f4811f7531d737bd35b2f3eacd3
omch-29 <chandwaniom29@gmail.com>
2026-01-26 20:35:38 +0530

    test

--- test1.cpp
+++ test1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     vector<int> nums = {9,4,5,3,45,67,88};
     int st=0,end=nums.size()-1;
-    while(st<end){
+    while(st<=end){
         swap(nums[st], nums[end]);
         st++;
         end--;
