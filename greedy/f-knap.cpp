eb90419750b19213c89831cc0c2cc9ba3ad4840d
omch-29 <chandwaniom29@gmail.com>
2026-02-07 17:23:25 +0530

    graph

---
+++ greedy/f-knap.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+bool compare(pair<double,int>p1, pair<double,int>p2){
+    return p1.first>p2.first;
+}
+int fractionalKnapSack(vector<int>val, vector<int> wt, int w){
+    int n=val.size();
+    vector<pair<double,int>>ratio(n,{0.0,0});
+    for(int i=0;i<n;i++){
+        ratio[i]={val[i]/(double)wt[i],i};
+    }
+    sort(ratio.begin(),ratio.end(),compare);
+    int ans=0;
+    for(int i=0;i<n;i++){
+        int idx=ratio[i].second;
+        if(wt[idx]<w){
+            ans+=val[idx];
+            w-=w;
+        }else{
+            ans+=ratio[i].first * w;
+            w=0;
+            break;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int>val={60,100,120};
+    vector<int>weight={10,20,30};
+    int w=50;
+    cout<<fractionalKnapSack(val,weight,w);
+}
