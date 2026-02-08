f8e4e66a4fa6355e00e41e40717c0139dcb84329
omch-29 <chandwaniom29@gmail.com>
2026-02-08 19:25:53 +0530

    bit

--- bitManipulation/oddEven.cpp
+++ bitManipulation/oddEven.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 #include<cstring>
+#include<algorithm>
+#include<string>
 #include<unordered_map>
 #include <climits>
 using namespace std;
@@ -11,6 +13,14 @@ int main(){
     int n;
     cout<<"enter:";
     cin>>n;
-    if((n&1)==0) cout<<"even";
-    else cout<<"odd";
+    // if((n&1)==0) cout<<"even";
+    // else cout<<"odd";
+    // cout<<(n>>1);
+    string res;
+    while(n>0){
+        res+=char('0'+(n&1));
+        n=n>>1;
+    }
+    reverse(res.begin(),res.end());
+    cout<<res;
 }
