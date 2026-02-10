cdc2d0bcbc7ba4b421ca5e1b6251144927e3cd12
omch-29 <chandwaniom29@gmail.com>
2026-02-10 19:51:56 +0530

    sw

---
+++ slidingWindow/c1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<algorithm>
+#include<string>
+#include<unordered_map>
+#include <climits>
+using namespace std;
+double sw(vector<int>&nums, int k){
+    double windowSum=0,maxSum=0;
+    for(int i=0;i<k;i++){
+        windowSum+=nums[i];
+    }
+    maxSum=windowSum;
+    for(int i=k;i<nums.size();i++){
+        windowSum+=nums[i];
+        windowSum-=nums[i-k];
+        maxSum=max(maxSum,windowSum);
+    }
+    return maxSum/4;
+}
+int main(){
+    vector<int>arr={1,12,-5,-6,50,3};
+    cout<<sw(arr,4);
+}
