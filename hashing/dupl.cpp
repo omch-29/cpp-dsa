dbcde1e666d14a8ff78b7c7e8fc6fed371c7914d
omch-29 <chandwaniom29@gmail.com>
2026-02-15 22:16:35 +0530

    hash

--- hashing/dupl.cpp
+++ hashing/dupl.cpp
@@ -11,12 +11,13 @@ using namespace std;
 int fd(vector<int>& nums){
     int dup=-1;
     for(int i=0;i<nums.size();i++){
-        int idx=abs(nums[i]-1);
+        int idx=abs(nums[i]-1);     //get the index of that element
         if(nums[idx]<0) return nums[i];
         else nums[idx]=-nums[idx];
     }
     return -1;
 }
 int main(){
-    vector<int>arr={1,2,2,4};
+    vector<int>arr={1,2,3,3,4};
+    cout<<fd(arr);
 }
