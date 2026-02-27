1ebf4c00bd1eed1821965371e94b81b8cd44ab65
omch-29 <chandwaniom29@gmail.com>
2026-02-27 11:48:15 +0530

    string

---
+++ strings/removeAdjace.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include<cstring>
+#include<stack>
+#include <climits>
+#include<algorithm>
+using namespace std;
+
+string removeDuplicates(string s){
+    string res;
+    for(char c:s){
+        if(!res.empty() && res.back()==c) res.pop_back();
+        else res.push_back(c);
+    }
+    return res;
+}
+string removeDuplicates2(string s){
+    stack<char>st;
+    for(char c:s){
+        if(!st.empty() && st.top()==c) st.pop();
+        else st.push(c);
+    }
+    string res;
+    while(!st.empty()){
+
+     res.push_back(st.top()); st.pop();
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+int main(){
+    cout<<removeDuplicates2("azxxzy");
+}
