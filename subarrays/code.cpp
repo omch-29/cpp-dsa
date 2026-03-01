87fa8ce18f1d4be33f30d4096c62c9ddeea02f8f
omch-29 <chandwaniom29@gmail.com>
2026-03-01 18:23:03 +0530

    subarrays

--- subarrays/code.cpp
+++ subarrays/code.cpp
@@ -9,10 +9,14 @@ int main(){
     //         cnt++;
     //     }
     // }
-    for(int i=0;i<nums.size();i++){
-        for(int j=i;j<nums.size();j++){
-            cnt++;
-        }
+    // for(int i=0;i<nums.size();i++){
+    //     for(int j=i;j<nums.size();j++){
+    //         cnt++;
+    //     }
+    // }
+    int l=0;
+    for(int r=0;r<nums.size();r++){
+        cnt+=r-l+1;
     }
     cout<<cnt;
 }
---
+++ subarrays/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+for(int i=0;i<nums.size();i++){
+    //     for(int j=i;j<nums.size();j++){
+    //         cnt++;
+    //     }
+    // }
