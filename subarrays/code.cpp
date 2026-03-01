415905b0db25c647ea2a87501838c64af605a7be
omch-29 <chandwaniom29@gmail.com>
2026-03-01 18:16:13 +0530

    subarrays

---
+++ subarrays/code.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> nums = {1,2,3,4};
+    int cnt=0;
+    // for(int i=0;i<nums.size();i++){
+    //     for(int j=0;j<=i;j++){
+    //         cnt++;
+    //     }
+    // }
+    for(int i=0;i<nums.size();i++){
+        for(int j=i;j<nums.size();j++){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+}
