514f493123cba537dd4a3d4c5a29ba389f6a9905
omch-29 <chandwaniom29@gmail.com>
2026-03-02 22:02:53 +0530

    RSA

--- Array/MinInRSA.cpp
+++ Array/MinInRSA.cpp
@@ -4,25 +4,23 @@ using namespace std;
 int search(vector<int>& nums){
     int st=0,end=nums.size()-1;
     while(st<end){
-        int mid = st+(end-st)/2;
+        int mid=st+(end-st)/2;
         if(nums[mid]<nums[end]) end=mid;
         else st=mid+1;
     }
-    cout<<nums[end]<<endl;
     return nums[st];
 }
 int search2(vector<int>& nums){
     int st=0,end=nums.size()-1;
     while(st<end){
-        int mid = st+(end-st)/2;
+        int mid=st+(end-st)/2;
         if(nums[mid]>nums[st]) st=mid+1;
         else end=mid;
     }
-    cout<<nums[end]<<endl;
     return nums[st];
 }
 int main(){
-    vector<int> nums = {6,7,8,9,10,11,12,13,14,0,1,2,3,4,5};
+    vector<int> nums = {6,7,8,9,10,11,12,13,14,15,16,2,3,4,5};
     cout<<search(nums)<<endl;
     cout<<search2(nums);
 }
---
+++ Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    // cout<<search2(nums);
