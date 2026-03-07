136a5af2cde1ad667330a36472a082a253ad50a1
omch-29 <chandwaniom29@gmail.com>
2026-03-07 17:00:40 +0530

    string

---
+++ strings/addString.cpp
@@ -0,0 +1,14 @@
+//num1 = "11", num2 = "123" = "134";
+#include<bits/stdc++.h>
+using namespace std;
+
+string addStrings(string num1, string num2){
+    int carry=0;
+    int i=num1.size()-1,j=num2.size();
+    while(i>=0 || j>=0 || carry){
+        int sum=carry;
+    }
+}
+int main(){
+    cout<<addStrings("456", "77");
+}
