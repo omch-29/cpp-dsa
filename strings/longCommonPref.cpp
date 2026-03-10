bf6e89fdb856098c853651fc641b08dcfa8b5c0b
omch-29 <chandwaniom29@gmail.com>
2026-03-10 18:38:34 +0530

    14

---
+++ strings/longCommonPref.cpp
@@ -0,0 +1,11 @@
+// ["flower","flow","flight"] = fl
+#include<bits/stdc++.h>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs){
+
+}
+int main(){
+    vector<string> strs = {"flower","flow","flight"};
+    cout<<longestCommonPrefix(strs);
+}
