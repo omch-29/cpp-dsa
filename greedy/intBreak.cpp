80644df158a99ddb3ed105694d37b6839b17465d
omch-29 <chandwaniom29@gmail.com>
2026-03-20 14:20:28 +0530

    greedy

---
+++ greedy/intBreak.cpp
@@ -0,0 +1,12 @@
+//Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+//Return the maximum product you can get.
+
+
+
+
+
+
+int main(){
+    int n = 10;     //36
+}
